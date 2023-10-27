c3f4ace07ff7a77023dbacceccc6546b4e30f0b3
hanwangyu <hanwangyu@foxmail.com>
2023-10-27 16:48:26 +0800

    feat: fake data

---
+++ assets/data/home.ts
@@ -0,0 +1,132 @@
+export const categories =  [
+  {
+    "name": "Acai",
+    "count": 7
+  },
+  {
+    "name": "African",
+    "count": 9
+  },
+  {
+    "name": "Alcohol",
+    "count": 330
+  },
+  {
+    "name": "American",
+    "count": 201
+  },
+  {
+    "name": "BBQ",
+    "count": 73
+  },
+  {
+    "name": "Brunch",
+    "count": 118
+  },
+  {
+    "name": "Cakes",
+    "count": 36
+  },
+  {
+    "name": "Dessert",
+    "count": 282
+  },
+  {
+    "name": "Falafel",
+    "count": 30
+  },
+  {
+    "name": "German",
+    "count": 5
+  },
+  {
+    "name": "Healthy",
+    "count": 247
+  },
+  {
+    "name": "Indian",
+    "count": 112
+  },
+  {
+    "name": "Meal Deal",
+    "count": 129
+  },
+  {
+    "name": "Pancakes",
+    "count": 15
+  },
+  {
+    "name": "Pizza",
+    "count": 208
+  },
+  {
+    "name": "Soup",
+    "count": 8
+  },
+  {
+    "name": "Takeaways",
+    "count": 87
+  },
+  {
+    "name": "Wraps",
+    "count": 99
+  }
+];
+// export const categories = [
+//   {
+//     text: 'Restaurants',
+//     img: require('@/assets/data/c1.png'),
+//   },
+//   {
+//     text: 'Grocery',
+//     img: require('@/assets/data/c2.png'),
+//   },
+//   {
+//     text: 'Offers',
+//     img: require('@/assets/data/c3.png'),
+//   },
+//   {
+//     text: 'Pickup',
+//     img: require('@/assets/data/c4.png'),
+//   },
+//   {
+//     text: 'HOP',
+//     img: require('@/assets/data/c5.png'),
+//   },
+//   {
+//     text: 'Pharmacy',
+//     img: require('@/assets/data/c6.png'),
+//   },
+// ];
+//
+// export const restaurants = [
+//   {
+//     name: 'Vapiano',
+//     rating: '4.5 Excellent',
+//     ratings: '(500+)',
+//     distance: '0.7 miles away',
+//     img: require('@/assets/data/r1.jpeg'),
+//     tags: ['Italian', 'Pizza', 'Pasta', 'Salads'],
+//     duration: '35 - 45',
+//   },
+//   {
+//     name: '✨Urban Greens✨',
+//     id: '2',
+//     rating: '4.9 Excellent',
+//     ratings: '(500+)',
+//     distance: '1.7 miles away',
+//     img: require('@/assets/data/r2.jpeg'),
+//     tags: ['Salads', 'Vegan', 'Healthy', 'British'],
+//     duration: '15 - 30',
+//   },
+//   {
+//     name: 'El Minero',
+//     id: '3',
+//     rating: '4.5 Excellent',
+//     ratings: '(500+)',
+//     distance: '3 miles away',
+//     img: require('@/assets/data/r3.jpeg'),
+//     tags: ['Spanish', 'Salads', 'Tpas', 'Pasta'],
+//     duration: '25 - 45',
+//   },
+// ];

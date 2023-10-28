eeef5ade0987754be87f3379c070df63b1eefb82
hanwangyu <hanwangyu@foxmail.com>
2023-10-28 19:19:47 +0800

    feat: GooglePlacesAutocomplete done

--- app/(modal)/location-search.tsx
+++ app/(modal)/location-search.tsx
@@ -3,8 +3,8 @@ import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import MapView from "react-native-maps";
 import Colors from "../../constants/Colors";
 import {useNavigation} from "expo-router";
-
-// process.env.EXPO_PUBLIC_GOOGLE_API_KEY
+import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
+import {Ionicons} from "@expo/vector-icons";
 
 const LocationSearch = () => {
     const navigation = useNavigation();
@@ -17,6 +17,43 @@ const LocationSearch = () => {
 
     return (
         <View style={{ flex: 1 }}>
+            <GooglePlacesAutocomplete
+                placeholder='Search or move the map'
+                fetchDetails={true}
+                onPress={(data, details) => {
+                    const point = details?.geometry.location;
+                    if (!point) return;
+                    setLocation({
+                        ...location,
+                        latitude: point.lat,
+                        longitude: point.lng,
+                    });
+                }}
+                query={{
+                    // key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+                    key: "your_api",
+                    language: 'en',
+                }}
+                renderLeftButton={() => (
+                    <View style={styles.boxIcon }>
+                        <Ionicons name={"search-outline"} size={24} color={Colors.medium} />
+                    </View>
+                )}
+                styles={{
+                    container: {
+                        flex: 0,
+                    },
+                    textInput: {
+                        backgroundColor: Colors.grey,
+                        paddingLeft: 35,
+                        borderRadius: 10,
+                    },
+                    textInputContainer: {
+                         backgroundColor: "#fff",
+                         padding: 8,
+                    },
+                }}
+            />
             <MapView showsUserLocation={true} style={styles.map} region={location} />
 
             <View style={styles.absoluteBox}>
@@ -50,6 +87,12 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontWeight: "bold",
         fontSize: 16,
+    },
+    boxIcon: {
+        position: "absolute",
+        left: 15,
+        top: 18,
+        zIndex: 1,
     },
 })
 

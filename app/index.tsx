eed8fc3299fc4b702e44116101c42ff725e6e50e
hanwangyu <hanwangyu@foxmail.com>
2023-10-29 12:04:54 +0800

    feat: home view done with horizontal scroll

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
+import Categories from "../components/categories";
+import Restaurants from "../components/restaurants";
+import Colors from "../constants/Colors";
 
 const Page = () => {
     return (
-        <View>
-            <Text>
-                Page
-            </Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
+                <Categories />
+                <Text style={styles.header}>
+                    Top picks in your neighbourhood
+                </Text>
+                <Restaurants />
+                <Text style={styles.header}>
+                    Restaurants Near you
+                </Text>
+                <Restaurants />
+            </ScrollView>
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        top: 100,
+        backgroundColor: Colors.lightGrey,
+    },
+    header: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 10,
+        paddingHorizontal: 16,
+        marginTop: 16,
+    },
+})
 
 export default Page;
